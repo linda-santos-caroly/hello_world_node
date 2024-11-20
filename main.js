3902aed7affc0c690bcc633b9567a570b2d0475c
linda-santos-caroly <linda.santos.caroly@gmail.com>
2024-11-20 17:02:08 -0300

    cria entpoint e adiciona dockerfile
    Signed-off-by: linda-santos-caroly <linda.santos.caroly@gmail.com>

---
+++ main.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const path = __dirname + '/front/';
+
+app.listen(8080,()=> {
+    console.log('listen port 8000');
+});
+
+app.get('/hello_world', (req,res)=>{
+    res.send('Oi, meu nome é Linda Santos');
+});
+
+app.get('/',(req,res) => {
+    res.sendFile(path + 'index.html')
+});
